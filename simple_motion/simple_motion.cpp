35636a3b7c019695ccebc50e654c3ae045e5c978
KNCheung <beobachter70@163.com>
2016-12-10 11:50:21 +0800

    基本功能完成

--- simple_motion/simple_motion.cpp
+++ simple_motion/simple_motion.cpp
@@ -6,6 +6,11 @@
 #include <native/task.h>
 #include <native/timer.h>
 
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
 
 // Data Type
 
@@ -31,143 +36,222 @@ typedef struct
 
 RT_TASK task_trajectory_generator;
 RT_TASK task_command_sender;
+RT_TASK task_debug;
 
 tPosCmd new_cmd;
-tAxisSetpoint axis1_setpoint;
+tAxisSetpoint axis1;
+
+const double INTERPOLATION_PERIOD = 0.001;
+const RTIME RT_TIME_FREQ = 1000000000;
+
+const RTIME PROGRAM_START_TIME = rt_timer_read();
 
 int cycle_count = 0;
 
 
 
-void task_trajectory_generator_proc(void *arg)
-{
-	RTIME now, previous;
+void task_trajectory_generator_proc(void *arg) {
+    static double now{0}, t0{0}, t1{0}, t2{0}, t3{0};
+    static tAxisSetpoint s0{0, 0}, s1{0, 0}, s2{0, 0}, s3{0, 0};
 
-	/*
-	 * Arguments: &task (NULL=self),
-	 *            start time,
-	 *            period (here: 1 s)
-	 */
-	rt_task_set_periodic(NULL, TM_NOW, 1000000000);
-	previous = rt_timer_read();
+//	rt_task_set_periodic(NULL, TM_NOW, (RTIME)(RT_TIME_FREQ * INTERPOLATION_PERIOD));
+    rt_task_set_periodic(NULL, TM_NOW, 100000000);
 
-        axis1_setpoint.Position = 0;
-        axis1_setpoint.Velocity = 0;
+    axis1.Position = 0;
+    axis1.Velocity = 0;
 
-        int temp;
+    tPosCmd target;
+    enum Interpolation_State {Idle, Acc, Uniform, Dec, End};
 
-	while (1) {
-		rt_task_wait_period(NULL);
-		now = rt_timer_read();
+    Interpolation_State state = Idle;
 
-		/*
-		 * NOTE: printf may have unexpected impact on the timing of
-		 *       your program. It is used here in the critical loop
-		 *       only for demonstration purposes.
-		 */
-		printf("Task A Time since last turn: %ld.%06ld ms\n",
-		       (long)(now - previous) / 1000000,
-		       (long)(now - previous) % 1000000);
-		       previous = now;
+    while (1) {
+        rt_task_wait_period(NULL);
+        now = rt_timer_read() / (double) RT_TIME_FREQ;
 
-               //  Add your code
-               if(new_cmd.Request)
-               {
-                  new_cmd.Response = true;
-                  new_cmd.Done = true;
-               }
-	}
+        switch (state) {
+            // Waiting for command
+            case Idle:
+                if (new_cmd.Request){
+                    target = new_cmd;
+                    new_cmd.Request = false;
+                    state = Acc;
+                    t0 = now;
+                    s0 = axis1;
+
+                    t1 = t0 + target.Velocity / target.Acceleration;
+                    s1.Position = s0.Position + target.Velocity * target.Velocity / (2 * target.Acceleration);
+                    s1.Velocity = target.Velocity;
+
+                    t2 = t1 +  (target.Position - axis1.Position) / target.Velocity
+                         - target.Velocity / (2 * target.Acceleration)
+                         - target.Velocity / (2 * target.Deceleration);
+                    s2.Position = s1.Position + target.Velocity * (t2 - t1);
+                    s2.Velocity = target.Velocity;
+
+                    t3 = t2 + target.Velocity / target.Deceleration;
+                    s3.Position = target.Position;
+                    s3.Velocity = 0;
+                    std::cerr << "switch to state <Acc>" << std::endl;
+                    // No Break; accelerate at once
+                } else {
+                    break;
+                }
+
+            // Accelerating
+            case Acc:
+                if (now >= t1) {
+                    axis1.Velocity = s1.Velocity;
+                    axis1.Position = s1.Position;
+                    state = Uniform;
+                    // No Break; Linear Uniform Moving at once
+                    std::cerr << "switch to state <Uniform>" << std::endl;
+                } else {
+                    axis1.Velocity = target.Velocity * (now - t0) / (t1 - t0);
+                    axis1.Position = s0.Position + 0.5 * target.Acceleration * (now - t0) * (now - t0);
+                    break;
+                }
+
+            // Linear Uniform Moving
+            case Uniform:
+                if (now >= t2) {
+                    axis1.Position = s2.Position;
+                    axis1.Velocity = s2.Velocity;
+                    state = Dec;
+                    // No Break; Decelerating at once
+                    std::cerr << "switch to state <Dec>" << std::endl;
+                } else {
+                    axis1.Velocity = target.Velocity;
+                    axis1.Position = s1.Position + s1.Velocity * (now - t1);
+                    break;
+                }
+
+            // Decelerating
+            case Dec:
+                if (now >= t3) {
+                    axis1.Position = s3.Position;
+                    axis1.Velocity = s3.Velocity;
+                    state = End;
+                    // No Break; Stop at once
+                    std::cerr << "switch to state <End>" << std::endl;
+                } else {
+                    axis1.Velocity = s2.Velocity - target.Deceleration * (now - t2);
+                    axis1.Position = s2.Position + s2.Velocity * (now - t2)
+                                     - 0.5 * target.Deceleration * (now - t2) * (now - t2);
+                    break;
+                }
+
+            // Stop
+            case End:
+                new_cmd.Done = true;
+                state = Idle;
+                std::cerr << "switch to state <Idle>" << std::endl;
+                break;
+
+            // Should not reach here
+            default:
+                throw new std::runtime_error("Reach Unknown State");
+        }
+
+    }
 }
 
 void task_command_sender_proc(void *arg)
 {
-	RTIME now, previous;
+    rt_task_set_periodic(NULL, TM_NOW, 2000000000);
 
-	/*
-	 * Arguments: &task (NULL=self),
-	 *            start time,
-	 *            period (here: 2 s)
-	 */
-	rt_task_set_periodic(NULL, TM_NOW, 2000000000);
-	previous = rt_timer_read();
+    new_cmd.Request = false;
+    new_cmd.Response = false;
+    new_cmd.Done = false;
+    new_cmd.Position = 0;
+    new_cmd.Velocity = 0;
+    new_cmd.Acceleration = 0;
+    new_cmd.Deceleration = 0;
+    new_cmd.Jerk = 0;
 
-        new_cmd.Request = false;
-        new_cmd.Response = false;
-        new_cmd.Done = false;
-        new_cmd.Position = 0;
-        new_cmd.Velocity = 0;
-        new_cmd.Acceleration = 0;
-        new_cmd.Deceleration = 0;
-        new_cmd.Jerk = 0;
+    while (1) {
+        rt_task_wait_period(NULL);
 
-	while (1) {
-		rt_task_wait_period(NULL);
-		now = rt_timer_read();
+        cycle_count = cycle_count + 1;
 
-		/*
-		 * NOTE: printf may have unexpected impact on the timing of
-		 *       your program. It is used here in the critical loop
-		 *       only for demonstration purposes.
-		 */
-		printf("Task B Time since last turn: %ld.%06ld ms\n",
-		       (long)(now - previous) / 1000000,
-		       (long)(now - previous) % 1000000);
-		       previous = now;
-                cycle_count = cycle_count + 1;
-                printf("cycle_count:%d\n",cycle_count);
-        
-                if(cycle_count == 5)
-                {
-                    new_cmd.Request = true;
-                    new_cmd.Response = false;
-                    new_cmd.Done = false;
-                    new_cmd.Position = 200000;
-                    new_cmd.Velocity = 1000;
-                    new_cmd.Acceleration = 50;
-                    new_cmd.Deceleration = 50;
-                    new_cmd.Jerk = 0;
+        if(cycle_count == 1)
+        {
+            new_cmd.Request = true;
+            new_cmd.Response = false;
+            new_cmd.Done = false;
+            new_cmd.Position = 200000;
+            new_cmd.Velocity = 1000;
+            new_cmd.Acceleration = 50;
+            new_cmd.Deceleration = 50;
+            new_cmd.Jerk = 0;
+        }
 
-                }
+    }
+}
 
-	}
+/**
+ * @brief print the current position & velocity to debug.log every 0.1s
+ * @param arg
+ */
+void task_debug_proc(void *arg) {
+    rt_task_set_periodic(NULL, TM_NOW, (RTIME) (RT_TIME_FREQ * 0.1));
+
+    std::ofstream log{"debug.log"};
+
+    printf("Task Debug Init\n");
+    while(1) {
+        rt_task_wait_period(NULL);
+        auto curr = axis1;
+        auto now = rt_timer_read() / (double) RT_TIME_FREQ;
+//		printf("%0.6f\t%0.6f\t%0.6f\n", now, curr.Position, curr.Velocity);
+        log << std::setiosflags(std::ios::fixed) << now << '\t' << curr.Position << '\t' << curr.Velocity << std::endl;
+        log.flush();
+    }
 }
 
 void catch_signal(int sig)
 {
-
 }
 
 
 int main(int argc, char* argv[])
 {
-	signal(SIGTERM, catch_signal);
-	signal(SIGINT, catch_signal);
+    signal(SIGTERM, catch_signal);
+    signal(SIGINT, catch_signal);
 
-	/* Avoids memory swapping for this program */
-	mlockall(MCL_CURRENT|MCL_FUTURE);
+    /* Avoids memory swapping for this program */
+    mlockall(MCL_CURRENT|MCL_FUTURE);
 
-        printf("A simple motion control demo\n");
-	/*
-	 * Arguments: &task,
-	 *            name,
-	 *            stack size (0=default),
-	 *            priority,
-	 *            mode (FPU, start suspended, ...)
-	 */
-	rt_task_create(&task_trajectory_generator, "rttask_trajectory_generator", 0, 98, 0);
-	rt_task_create(&task_command_sender, "rttask_command_sender", 0, 99, 0);
+    printf("A simple motion control demo\n");
+    /*
+     * Arguments: &task,
+     *            name,
+     *            stack size (0=default),
+     *            priority,
+     *            mode (FPU, start suspended, ...)
+     */
+    rt_task_create(&task_trajectory_generator, "rttask_trajectory_generator", 0, 90, 0);
+    rt_task_create(&task_command_sender, "rttask_command_sender", 0, 92, 0);
+    rt_task_create(&task_debug, "rttask_debug", 0, 95, 0);
 
-	/*
-	 * Arguments: &task,
-	 *            task function,
-	 *            function argument
-	 */
-	rt_task_start(&task_trajectory_generator, &task_trajectory_generator_proc, NULL);
-	rt_task_start(&task_command_sender, &task_command_sender_proc, NULL);
+    /*
+     * Arguments: &task,
+     *            task function,
+     *            function argument
+     */
+    rt_task_start(&task_trajectory_generator, &task_trajectory_generator_proc, NULL);
+    rt_task_start(&task_command_sender, &task_command_sender_proc, NULL);
+    rt_task_start(&task_debug, &task_debug_proc, NULL);
 
-        while(!new_cmd.Done);
-        printf("End! \n");
-	rt_task_delete(&task_trajectory_generator);
-	rt_task_delete(&task_command_sender);
-	return 0;
+//    pause();
+    int cnt = 0;
+    while (!new_cmd.Done) {
+        rt_timer_spin(1000000000);
+        printf("tick: %d\n", ++cnt);
+    }
+    printf("End! \n");
+    rt_task_delete(&task_trajectory_generator);
+    rt_task_delete(&task_command_sender);
+    rt_task_delete(&task_debug);
+    return 0;
 }
